1ed37c361d560f8bc97ce5bf0eb60287d55ddeba
Audyari Wiyono <audy123ari@gmail.com>
2026-06-01 07:25:36 +0700

    Tipe Data Number

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        byte iniByte = 100;
+        short iniShort = 100;
+        int iniInt = 100;
+        long iniLong = 100;
+        float iniFloat = 100;
+        double iniDouble = 100;
+        char iniChar = 'a';
+        boolean iniBool = true;
+
+        System.out.println(iniByte);
+        System.out.println(iniShort);
+        System.out.println(iniInt);
+        System.out.println(iniLong);
+        System.out.println(iniFloat);
+        System.out.println(iniDouble);
+        System.out.println(iniChar);
+        System.out.println(iniBool);
+
+    }
+}
